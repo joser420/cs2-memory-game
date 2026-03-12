94d71d2a757dd31565f6471a7d07c834443adf91
joser420 <0815231022@s.tusd1.org>
2026-03-12 16:27:10 +0000

    i tried my best

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 console.log("Script started");
+let guesses = 0;
+
+let firstCardId = "";
+
+let secondCardId = "";
+
+function flipCard(CardId){
+    console.log("click")
+    let card =document.getElementById(CardId);
+
+    card.style.color = "white";
+
+    card.style.backgroundColor = "cornflowerblue";
+    if (firstCardId == ""){
+        firstCardId = CardId;
+        console.log(firstCardId);
+    }
+
+    else {
+        secondCardId = CardId;
+        guesses = guesses + 1;
+        console.log(secondCardId);
+        console.log("guesses = " + guesses);
+        let guessDisplay = document.getElementById("guesses");
+        guessDisplay.innerText = "Guesses: " + guesses;
+
+    }
+}
+
+function checkForMatch(){
+    let card1 = document.getElementById(firstCardId);
+    let card2 = document.getElementById(secondCardId)
+    if (card1.innerText == card2.innerText){
+        card1.style.backgroundColor = "green";
+        card2.style.backgroundColor = "green";
+    }
+
+    else {
+        
+    }
+
+}
